2fc9c9438037b28a83a5c76668fdfe7ba9987f0b
Elan Rasabi <elan.rasabi@tinroofsoftware.com>
2020-08-21 16:12:18 -0400

    #6b [Update Component onClick(commit 6A is first half!): commit 6A but solves infinite loop and initial state.prop null case

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -8,10 +8,21 @@ class FullPost extends Component {
     }
     componentDidUpdate() {
         if (this.props.id) {
-            axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
-                .then( response => {
-                    this.setState({loadedPost: response.data})
-                });
+            // PROBLEM:
+            //  httpReq will repeat infinitely because setState change will cause rerender
+            //  which will rerender componentDidUpdate which will rerender setState etc ...
+            // SOLUTION: 
+            // 1) add `this.state.loadedPost && this.state.loadedPost.id !== this.props.id` to stop infinite loop
+                //  httpReq is run again then postId value will be the same as was previously set to loadedPost.id
+                //  by checking if postId is the same as loadedPost.id we verify the httpReq is not being run again
+            // 2) also add `!this.state.loadedPost || ` or else:
+                // the httpReq will never run because loadedPost is null and will never meet other conditions
+            if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+                axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+                    .then(response => {
+                        this.setState({ loadedPost: response.data })
+                    });
+            }
         }
     }
     render () {
