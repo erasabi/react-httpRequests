b6a108b63311729bd2068ffcf696fa581984fc57
Elan Rasabi <elan.rasabi@tinroofsoftware.com>
2020-08-20 21:08:48 -0400

    npm build changes

---
+++ build/precache-manifest.b4ea95c2cac52fad26ea67e45011bc75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfb147f8adad20465c8ec9c8ae1c606b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a043ab5f450af21d5194",
+    "url": "/static/css/main.22ba39bb.chunk.css"
+  },
+  {
+    "revision": "2ebcd59761dd2280cdbd",
+    "url": "/static/js/2.db24ece2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.db24ece2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a043ab5f450af21d5194",
+    "url": "/static/js/main.be13f2ce.chunk.js"
+  },
+  {
+    "revision": "5132f4db0caab9ba715b",
+    "url": "/static/js/runtime-main.30538d72.js"
+  }
+]);
